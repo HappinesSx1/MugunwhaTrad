354f79c41ecd253c9ae944e31d5e5dec24f64804
unknown <david.b57970@laposte.net>
2024-03-02 00:13:14 +0100

    serveur local nodejs fini et route get

---
+++ backend/models/webtoon.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+  },
+});
+
+const webtoonSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    chapitres: [imageSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("webtoon", webtoonSchema);
